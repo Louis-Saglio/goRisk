24aab03108de33dcae56cd823a06b6249c63b66f
Louis-Saglio <louis.saglio@sfr.fr>
2017-11-02 23:18:32 +0100

    Développement synchrone terminé

--- main.go
+++ main.go
@@ -40,8 +40,46 @@ func combattre(nbrAttaquants int, nbrDefenseurs int) (string, int, int) {
 		}
 	}
 
-	fmt.Println(nbrAttaquants, nbrDefenseurs)
-	return "nil", 0, 0
+	vainqueur := "defenseur"
+	if nbrDefenseurs == 0 {
+		vainqueur = "attaquant"
+	}
+
+	return vainqueur, nbrAttaquants, nbrDefenseurs
+}
+
+
+func resultatMoyen(nbrAttaquants int, nbrDefenseurs int, nbrSimulations int) (string, float64, float64, float64, float64){
+	totalVictoiresAttaquant, totalVictoiresDefenseur, totalResteAttaquant, totalResteDefenseur := 0, 0, 0, 0
+	for i := 0; i < nbrSimulations; i++ {
+		vainqueur, resteAttaquant, resteDefenseur := combattre(nbrAttaquants, nbrDefenseurs)
+		if vainqueur == "attaquant" {
+			totalVictoiresAttaquant += 1
+		} else {
+			totalVictoiresDefenseur += 1
+		}
+		totalResteAttaquant += resteAttaquant
+		totalResteDefenseur += resteDefenseur
+	}
+	probaVictoireAttaquant := float64(totalVictoiresAttaquant) / float64(nbrSimulations)
+	probaVictoireDefenseur := float64(totalVictoiresDefenseur) / float64(nbrSimulations)
+	vainqueur := "defenseur"
+	if probaVictoireAttaquant > probaVictoireDefenseur {
+		vainqueur = "attaquant"
+	}
+	return vainqueur, float64(totalResteAttaquant) / float64(nbrSimulations), float64(totalResteDefenseur) / float64(nbrSimulations), probaVictoireAttaquant, probaVictoireDefenseur
+}
+
+
+func trouverCombienGagnentContre(nbrDefenseurs int) (int, float64) {
+	nbrAttaquants := 1
+	vainqueur := "defenseur"
+	var probabiliteVictoireAttaquant float64
+	for vainqueur == "defenseur" {
+		nbrAttaquants += 1
+		vainqueur, _, _, probabiliteVictoireAttaquant, _ = resultatMoyen(nbrAttaquants, nbrDefenseurs, 100)
+	}
+	return nbrAttaquants, probabiliteVictoireAttaquant
 }
 
 func getNbrDesDefenseur(desAttaquant []int, nbrDefenseurs int) int {
@@ -55,12 +93,19 @@ func getNbrDesDefenseur(desAttaquant []int, nbrDefenseurs int) int {
 	return 2
 }
 
+
 func getNbrDesAttaquant(nbrAttaquants int) int {
 	return int(math.Trunc(float64(nbrAttaquants-1)))
 }
 
+
 func main() {
 	debut := time.Now().UTC().UnixNano() / 1000000
-	combattre(190, 720)
+
+	for i := 0; i < 100; i++ {
+		nbrAttaquants, probabilite := trouverCombienGagnentContre(i)
+		fmt.Println(nbrAttaquants, "gagnent contre", i, "probabilité :", probabilite)
+	}
+
 	fmt.Println(time.Now().UTC().UnixNano() / 1000000 - debut, "millisecondes")
 }
